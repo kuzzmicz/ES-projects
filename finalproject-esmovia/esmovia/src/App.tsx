f41498d4b84e9aa6f442cd4fdd3b43f3569a8567
kuzzmicz <kuzzmiczpies@gmail.com>
2024-07-10 13:05:43 +0200

    Lesson 10 - components Characters and Games are working and displaying info.

--- finalproject-esmovia/esmovia/src/App.tsx
+++ finalproject-esmovia/esmovia/src/App.tsx
@@ -1,44 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Game, Character } from './interfaces';
 
+import './App.css'
+import Footer from './common/Footer/Footer';
+import Header from './common/Header/Header';
+import Body from './pages/Body/Body';
 
-const App: React.FC = () => {
-  const [characters, setCharacters] = useState<Character[]>([]);
-  const [games, setGames] = useState<Game[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const charResponse = await axios.get('http://localhost:3000/api/characters');
-        setCharacters(charResponse.data);
-
-        const gameResponse = await axios.get('http://localhost:3000/api/games');
-        setGames(gameResponse.data);
-      } catch (error) {
-        console.error('Error fetching data', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+function App() {
 
   return (
-    <div>
-      <h1>Resident Evil Characters</h1>
-      <ul>
-        {characters.map(character => (
-          <li key={character.id}>{character.name} - {character.game}</li>
-        ))}
-      </ul>
-      <h1>Resident Evil Games</h1>
-      <ul>
-        {games.map(game => (
-          <li key={game.id}>{game.title} - {game.releaseYear}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+    <>
+    <Header/>
+     <Body/>
+    <Footer/>
+    </>
+  )
+}
 
 export default App;
--- finalproject-esmovia/esmovia/src/pages/Games/Games.tsx
+++ finalproject-esmovia/esmovia/src/pages/Games/Games.tsx
@@ -1,11 +1,29 @@
 import "./Games.css";
-
+import { Game } from '../../interfaces';
+import { useState, useEffect } from "react";
+import axios from 'axios';
 function Games(){
-
-
+    const [games, setGames] = useState<Game[]>([]);
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const gameResponse = await axios.get('http://localhost:3000/api/games');
+            setGames(gameResponse.data);
+          } catch (error) {
+            console.error('Error fetching data', error);
+          }
+        };
+    
+        fetchData();
+      }, []);
     return (
      <div className="games-design">
-         GAMES
+         <h1>Resident Evil Games</h1>
+      <ul>
+        {games.map(game => (
+          <li key={game.id}>{game.title} - {game.releaseYear}</li>
+        ))}
+      </ul>
      </div>
     )
 }
