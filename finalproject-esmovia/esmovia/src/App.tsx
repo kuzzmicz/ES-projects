9dd4afc083fccf05ff436899adfb066d8ac93cc8
kuzzmicz <kuzzmiczpies@gmail.com>
2024-07-10 10:42:33 +0200

    Lesson 10 - an example app.

--- finalproject-esmovia/esmovia/src/App.tsx
+++ finalproject-esmovia/esmovia/src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Answer, FetchTodo } from './interfaces';
+import bringTodo from './services/api-calls';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos, setTodos] = useState<FetchTodo[]>([])
+
+  useEffect(()=>{
+    if(todos.length === 0){
+
+      const bringTd = async (): Promise<void> => {
+        
+        let answer : Answer = await bringTodo()
+
+        console.log(answer, "i ammmmm")
+
+        setTodos(answer.data)  // --> Main thing of the function :)
+      } 
+
+      bringTd()
+    }
+  },[todos]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     {
+      todos?.length > 0 
+
+      ? 
+
+      (<div>{todos?.length}</div>)
+
+      :
+
+      (<div>nein!</div>)
+     }
     </>
   )
 }
 
-export default App
+export default App;
